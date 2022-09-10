904592ba8ea9ef93eecb5fb463db4792e93ef4db
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2022-09-09 17:10:35 -0700

    Update Inventory.cs

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -2,5 +2,16 @@
 {
     public class Inventory
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public float Quantity { get; set; }
+        public float PricePerUnit { get; set; }
+
+
+        public Inventory()
+        {
+
+        }
+
     }
 }
