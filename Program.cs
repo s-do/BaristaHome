0a2986ed108197fd8c8eccf6a4dca4ed26cc4348
borkbunns <petter.vu2000@gmail.com>
2022-09-14 02:30:04 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -64,7 +64,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
