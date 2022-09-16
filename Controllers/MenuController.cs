8cbeff6bc392233967e8b2bc1008ff82ef160016
EmeraldEnvy <EmeraldEnvy500@gmail.com>
2022-09-15 21:17:08 -0700

    Controller stuff

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,15 +1,19 @@
 ﻿using BaristaHome.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using BaristaHome.Data;
 
 namespace BaristaHome.Controllers
 {
     public class MenuController : Controller
     {
-        private readonly ILogger<MenuController> _logger;
-        public MenuController(ILogger<MenuController> logger)
+        private readonly BaristaHomeContext _context;
+
+        public MenuController(BaristaHomeContext context)
         {
-            _logger = logger;
+            _context = context;
         }
         public IActionResult Menu()
         {
@@ -26,5 +30,37 @@ namespace BaristaHome.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddItem([Bind("DrinkName,Instructions,Description,DrinkImage")] Drink home)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(home.DrinkImage);
+            home.DrinkImageData = bytes;
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(home);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Menu", "Menu");
+            }
+            ModelState.AddModelError(string.Empty, home.DrinkName);
+            return View(home);
+        }
+        public class Image
+        {
+            public int Id { get; set; }
+            public string ImageTitle { get; set; }
+            public byte[] ImageData { get; set; }
+        }
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Image> Images { get; set; }
+        }
+
+
     }
 }
