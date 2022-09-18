4b0a01de80165047a1ef410444e69624903e9090
borkbunns <petter.vu2000@gmail.com>
2022-09-17 19:46:42 -0700

    Successfully added inventoryitems into respective user's store, as well as display all of its items

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -24,16 +24,32 @@ namespace BaristaHome.Controllers
         }
 
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            /*  ViewBag.Units = new SelectList(_context.Unit.ToList(), "UnitName");*/
+            // dw, with the utilization of ItemViewModel we can display a store's inventory instead
+            // https://stackoverflow.com/questions/57727635/how-to-pass-selected-query-list-using-viewbag i used this link to display list of store's items
+            List<ItemViewModel> itemQuery = (from store in _context.Store
+                                             join inventory in _context.InventoryItem on store.StoreId equals inventory.StoreId // link store and inventoryitem by storeid
+                                             join item in _context.Item on inventory.ItemId equals item.ItemId                  // link inventoryitem and item by itemid
+                                             where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))       // filter items by user's store
+                                             select new ItemViewModel
+                                             {
+                                                    Name = item.ItemName,                  // now we can send a 
+                                                    Quantity = inventory.Quantity,         // ItemViewModel object
+                                                    PricePerUnit = inventory.PricePerUnit  // to the view
+                                             }).ToList();
+            ViewBag.Inventory = itemQuery;
+            return View();
+
+
+            /*  ViewBag.Units = new SelectList(_context.Unit.ToList(), "UnitName");*//*
 
             List<Unit> uni = new List<Unit>();
             uni = _context.Unit.ToList();
 
             List<SelectListItem> Units = new List<SelectListItem>();
             
-                /*new SelectListItem(){Text="Fluid Ounces",Value ="fl"}*/
+                *//*new SelectListItem(){Text="Fluid Ounces",Value ="fl"}*//*
             foreach( var u in uni)
             {
                 Units.Add(new SelectListItem
@@ -44,26 +60,82 @@ namespace BaristaHome.Controllers
             }
 
             ViewBag.UnitList = Units;
-            return View();
+            return View();*/
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index([Bind("ItemName")] Item item, [Bind("Quantity,PricePerUnit")] InventoryItem inventory)
+        public async Task<IActionResult> Index(ItemViewModel item)
         {
+            // Look at our properties for ItemViewModel.cs under the Models folder, you can see how I use ViewModels to generate a view to input
+            // those fields so I can validate it existing in the actual database tables in the controller here
 
+            if (ModelState.IsValid)
+            {
+                // Get the id of the unit name         
+                var unitId = (from u in _context.Unit
+                              where u.UnitName.Equals(item.UnitName)
+                              select u.UnitId).FirstOrDefault();
 
+                // Now check if the item name exists in the database
+                var existingItem = (from i in _context.Item
+                                    where i.ItemName.Equals(item.Name)
+                                    select i).FirstOrDefault();
 
-            // TODO: POST THAT USER SELECTION AND PUT IT IN HERE
-         
-            var unitId = (from u in _context.Unit
-                          where u.UnitName.Equals("UsersSelectedUnitInput")
-                          select u.UnitId).FirstOrDefault();
+                if (existingItem != null)
+                {
+                    // Okay the item exists in our Item table, now we check if this item is already in THIS STORE
+                    var existingInventoryItem = (from inv in _context.InventoryItem
+                                                 where inv.ItemId.Equals(existingItem.ItemId)
+                                                 select inv).FirstOrDefault();
 
-            //item.UnitId = unitId;
+                    if (existingInventoryItem != null)
+                    {
+                        ModelState.AddModelError(string.Empty, "You already have this item in your inventory! Use Edit to change the quantity instead.");
+                        return View(item); // We can sort of fix this later, the error above pops up when you reopen the modal (i don't know how to send the view back with it still open)
+                    }
 
-            item.UnitId = 1;
+                    // Alright so if the item exists in Item but it isn't inside InventoryItem yet, instantiate a new InventoryItem with the inputted fields and Add()
+                    var inventoryItem = new InventoryItem()
+                    {
+                        ItemId = existingItem.ItemId,
+                        StoreId = Convert.ToInt32(User.FindFirst("StoreId").Value),
+                        Quantity = item.Quantity,
+                        PricePerUnit = item.PricePerUnit
+                    };
+                    _context.Add(inventoryItem);
+                    await _context.SaveChangesAsync();
 
-            // First add the new item into the Item db
+                }
+                else
+                {
+                    // existingItem is null, so we add it into the Item table
+                    var newItem = new Item()
+                    {
+                        ItemName = item.Name,
+                        UnitId = unitId
+                    };
+                    _context.Add(newItem);
+                    await _context.SaveChangesAsync();
+
+                    // And then add it to the store's InventoryItem table
+                    var inventoryItem = new InventoryItem()
+                    {
+                        ItemId = newItem.ItemId, // The new item id we just added into the database
+                        StoreId = Convert.ToInt32(User.FindFirst("StoreId").Value),
+                        Quantity = item.Quantity,
+                        PricePerUnit = item.PricePerUnit
+                    };
+                    _context.Add(inventoryItem);
+                    await _context.SaveChangesAsync();
+                }
+                return RedirectToAction("Index", "Inventory"); // new InventoryItem should be successfully added into the user's store
+            }
+            ModelState.AddModelError(string.Empty, "There was an issue adding this item into your inventory.");
+            return View();
+
+            // old code you can delete or save for reference ig
+
+            /*// First add the new item into the Item db
             _context.Add(item);
             // _context.SaveChanges();
             await _context.SaveChangesAsync(); // use this to save changes to the db instead, so the method pauses until this task completes
@@ -86,10 +158,9 @@ namespace BaristaHome.Controllers
                 // return RedirectToAction("Index"); <-- the syntax of this method is RedirectToAction("ActionName", "ControllerName");
 
                 return View();
-            }
+            }*/
 
-            ModelState.AddModelError(string.Empty, "There was an issue adding an item.");
-            return View();
+
         }
 
 
---
+++ Models/ItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaristaHome.Models
+{
+    public class ItemViewModel
+    {
+        public string Name { get; set; }
+
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Only two decimals cuck")] // This ensures the property has at most 2 decimal places
+        [Range(0, 99999999999999.99)]
+        public decimal Quantity { get; set; }
+
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Only two decimals cuck")]
+        [Range(0, 99999999999999.99)]
+        public decimal PricePerUnit { get; set; }
+
+        public string UnitName { get; set; }
+
+    }
+}
