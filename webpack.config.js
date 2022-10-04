4bfaa8be9b3a7916ec12e0eb42f4bfaeeb8f01d0
borkbunns <petter.vu2000@gmail.com>
2022-10-03 19:12:22 -0700

    added calendar

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/calendar.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    publicPath: '/',
+    filename: 'js/calendar.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/calendar.css'
+    })
+  ]
+}
