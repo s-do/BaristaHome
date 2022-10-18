536f98dc51d0f6f2bdc8da2cae63dee45d97df33
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2022-10-18 15:41:01 -0700

    Payroll
    Got to display payrolls of employees under same store ID

--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -1,6 +1,8 @@
-﻿using BaristaHome.Models;
+﻿using BaristaHome.Data;
+using BaristaHome.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BaristaHome.Controllers
@@ -8,16 +10,27 @@ namespace BaristaHome.Controllers
     [Authorize]
     public class PayrollController : Controller
     {
-        private readonly ILogger<PayrollController> _logger;
-        public PayrollController(ILogger<PayrollController> logger)
+        //private readonly ILogger<PayrollController> _logger;
+        private readonly BaristaHomeContext _context;
+
+
+        public PayrollController(BaristaHomeContext context)
+        {
+            _context = context;
+            //_logger = (ILogger<PayrollController>?)logger;
+        }
+
+
+/*        public PayrollController(ILogger<PayrollController> logger)
         {
             _logger = logger;
-        }
-        public IActionResult Owner()
+        }*/
+
+/*        public IActionResult Owner()
         {
             return View();
         }
-
+*/
         public IActionResult Worker()
         {
             return View();
@@ -28,5 +41,28 @@ namespace BaristaHome.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public async Task<IActionResult> Owner()
+        {
+            /*            var workers = await _context.Payroll
+                                        .Where(u => u.StoreId == Convert.ToInt16(User.FindFirst("StoreId").Value))
+                                        .OrderByDescending(u => u.RoleId)
+                                        .ThenBy(u => u.FirstName)
+                                        .ToListAsync();*/
+
+            var payrolls = (from payroll in _context.Payroll
+                           join user in _context.User on payroll.UserId equals user.UserId
+                           join store in _context.Store on user.StoreId equals store.StoreId
+                           where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
+                           select payroll
+                           ).ToList();
+
+            return View(payrolls);
+        }
+
+
+
+
     }
 }
