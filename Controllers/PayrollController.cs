2d3731aa1e35f11721955c0be53b8f4d395eb084
EmeraldEnvy <EmeraldEnvy500@gmail.com>
2022-10-18 20:04:23 -0700

    Displayed data

--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -1,30 +1,33 @@
-﻿using BaristaHome.Data;
-using BaristaHome.Models;
+﻿using BaristaHome.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using BaristaHome.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BaristaHome.Controllers
 {
     [Authorize]
     public class PayrollController : Controller
     {
-        private readonly ILogger<PayrollController> _logger;
         private readonly BaristaHomeContext _context;
-        public PayrollController(ILogger<PayrollController> logger)
+        public PayrollController(BaristaHomeContext context)
         {
-            _logger = logger;
+            _context = context;
         }
         public IActionResult Owner()
         {
             return View();
         }
 
-        public IActionResult Worker()
+/*        public IActionResult Worker()
         {
             return View();
-        }
+        }*/
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -41,17 +44,41 @@ namespace BaristaHome.Controllers
                 return NotFound();
             }
 
-            var worker = await _context.User.FirstOrDefault(m => m.UserId == id);
-            /*            var worker = (from u in _context.User
-                                        where u.UserId.Equals(id)
-                                        select u).FirstOrDefault();*/
-
+            var worker = await _context.User.FirstOrDefaultAsync(m => m.UserId == id);
 
             if (worker == null)
             {
                 return NotFound();
             }
+
+            List<Payroll> payrollQuery = (from u in _context.User
+                                     join payroll in _context.Payroll on u.UserId equals payroll.UserId
+                                     where payroll.UserId == worker.UserId
+                                     select payroll).ToList();
+            ViewBag.PayrollList = payrollQuery;
+
             return View(worker);
+        }
+
+        public async Task<ActionResult> RenderImage(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var worker = await _context.User
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+            var image = (from u in _context.User
+                         where u.UserId == worker.UserId
+                         select worker.UserImageData).First();
+
+
+            return File(image, "image/png");
         }
     }
 }
