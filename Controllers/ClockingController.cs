525c4a4deaee26061392ea28207f34d684cdd268
Selina Do <selina.do@student.csulb.edu>
2022-10-21 22:09:58 -0700

    made different shift status UI

--- Controllers/ClockingController.cs
+++ Controllers/ClockingController.cs
@@ -2,6 +2,8 @@
 using BaristaHome.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaristaHome.Controllers
 {
@@ -13,30 +15,97 @@ namespace BaristaHome.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
         public IActionResult Clocking()
         {
             // Getting current user's first name
             var firstName = (from u in _context.User
-                        where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
-                        select u.FirstName).FirstOrDefault();
+                             where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                             select u.FirstName).FirstOrDefault();
             ViewBag.FirstName = firstName;
 
 
             // Getting current user's last name
             var lastName = (from u in _context.User
-                             where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
-                             select u.LastName).FirstOrDefault();
+                            where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                            select u.LastName).FirstOrDefault();
 
             ViewBag.LastName = lastName;
 
             //Get all the shift status names
             List<ShiftStatus> shiftStatus = (from ss in _context.ShiftStatus
-                               select ss).ToList();
+                                             select ss).ToList();
             ViewBag.ShiftStatus = shiftStatus;
 
             return View();
         }
 
-        
+        [HttpGet]
+        public IActionResult ClockIn()
+        {
+            // Getting current user's first name
+            var firstName = (from u in _context.User
+                             where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                             select u.FirstName).FirstOrDefault();
+            ViewBag.FirstName = firstName;
+
+
+            // Getting current user's last name
+            var lastName = (from u in _context.User
+                            where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                            select u.LastName).FirstOrDefault();
+
+            ViewBag.LastName = lastName;
+
+            //Get all the shift status names
+            List<ShiftStatus> shiftStatus = (from ss in _context.ShiftStatus
+                                             select ss).ToList();
+            ViewBag.ShiftStatus = shiftStatus;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ClockingIn([Bind("ShiftStatusId")] UserShiftStatus userSS)
+        {
+            userSS.UserId = Convert.ToInt16(User.FindFirst("UserId").Value);
+            DateTime time = DateTime.Now;
+            userSS.Time = time;
+
+            /*_context.Add(userSS);
+            await _context.SaveChangesAsync();*/
+
+
+            //Get all the shift status names
+            List<ShiftStatus> shiftStatus = (from ss in _context.ShiftStatus
+                                             select ss).ToList();
+            ViewBag.ShiftStatus = shiftStatus;
+
+            return RedirectToAction("ClockIn", "Clocking");
+        }
+
+        [HttpGet]
+        public IActionResult StartBreak()
+        {
+            // Getting current user's first name
+            var firstName = (from u in _context.User
+                             where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                             select u.FirstName).FirstOrDefault();
+            ViewBag.FirstName = firstName;
+
+
+            // Getting current user's last name
+            var lastName = (from u in _context.User
+                            where u.UserId.Equals(Convert.ToInt16(User.FindFirst("UserId").Value))
+                            select u.LastName).FirstOrDefault();
+
+            ViewBag.LastName = lastName;
+
+            //Get all the shift status names
+            List<ShiftStatus> shiftStatus = (from ss in _context.ShiftStatus
+                                             select ss).ToList();
+            ViewBag.ShiftStatus = shiftStatus;
+            return View();
+        }
     }
 }
