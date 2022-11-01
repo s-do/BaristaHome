0a3c66e32b89e72e3e47e44da5791d6f031eae06
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2022-10-31 23:23:24 -0700

    Payroll Controller changes
    Made it not display duplicate names in drop down. But need to change the query to take from users table not distinct names in payroll table

--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -1,6 +1,7 @@
 ﻿using BaristaHome.Data;
 using BaristaHome.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -46,25 +47,116 @@ namespace BaristaHome.Controllers
 
         public async Task<IActionResult> Owner()
         {
+            /*            List<PayrollOwnerViewModel> payrolls = (from payroll in _context.Payroll
+                                                               join user in _context.User on payroll.UserId equals user.UserId
+                                                               join store in _context.Store on user.StoreId equals store.StoreId
+                                                               where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
+                                                               select new PayrollOwnerViewModel
+                                                               {
+                                                                   Hours = payroll.Hours,
+                                                                   Amount = payroll.Amount,
+                                                                   Date = payroll.Date,
+                                                                   UserId = user.UserId,
+                                                                   FullName = user.FirstName + " " + user.LastName,
+                                                               }).ToList();*/
+
             List<PayrollOwnerViewModel> payrolls = (from payroll in _context.Payroll
-                                                   join user in _context.User on payroll.UserId equals user.UserId
-                                                   join store in _context.Store on user.StoreId equals store.StoreId
-                                                   where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
-                                                   select new PayrollOwnerViewModel
-                                                   {
-                                                       Hours = payroll.Hours,
-                                                       Amount = payroll.Amount,
-                                                       Date = payroll.Date,
-                                                       UserId = user.UserId,
-                                                       FullName = user.FirstName + " " + user.LastName,
-                                                   }).ToList();
+                                                    join user in _context.User on payroll.UserId equals user.UserId
+                                                    join store in _context.Store on user.StoreId equals store.StoreId
+                                                    where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
+                                                    select new PayrollOwnerViewModel
+                                                    {
+                                                        Hours = payroll.Hours,
+                                                        Amount = payroll.Amount,
+                                                        Date = payroll.Date,
+                                                        UserId = user.UserId,
+                                                        FullName = user.FirstName + " " + user.LastName,
+                                                    }).ToList();
 
             ViewBag.Payroll = payrolls;
 
-            ViewData["FullNames"] = new SelectList(payrolls, "FullName", "FullName");
+
+
+            /* A list of non duplicate full names, takes the first payroll with that full name.  */
+            List<PayrollOwnerViewModel> diff_name_payrolls = new List<PayrollOwnerViewModel>();
+            diff_name_payrolls = payrolls
+                                .GroupBy(element => element.FullName)
+                                .Select(e => e.First())
+                                .ToList();
+
+
+            ViewData["FullNames"] = new SelectList(diff_name_payrolls, "UserId", "FullName");
 
             return View();
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Owner(PayrollOwnerViewModel payrollOwnerViewModel)
+        {
+            List<PayrollOwnerViewModel> payrolls = (from payroll in _context.Payroll
+                                                    join user in _context.User on payroll.UserId equals user.UserId
+                                                    join store in _context.Store on user.StoreId equals store.StoreId
+                                                    where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
+                                                    select new PayrollOwnerViewModel
+                                                    {
+                                                        Hours = payroll.Hours,
+                                                        Amount = payroll.Amount,
+                                                        Date = payroll.Date,
+                                                        UserId = user.UserId,
+                                                        FullName = user.FirstName + " " + user.LastName,
+                                                    }).ToList();
+            ViewBag.Payroll = payrolls;
+
+/*          To give a selectlist without any duplicate names*/
+            List<PayrollOwnerViewModel> diff_name_payrolls = new List<PayrollOwnerViewModel>();
+            diff_name_payrolls = payrolls
+                                .GroupBy(element => element.FullName)
+                                .Select(e => e.First())
+                                .ToList();
+
+/*            Shows the full name but when selected, it will give the userId as Value so database knows what user is being added*/
+            ViewData["FullNames"] = new SelectList(diff_name_payrolls, "UserId", "FullName");
+
+
+
+            if (ModelState.IsValid)
+            {
+                var payroll1 = new Payroll()
+                {
+                    Hours = payrollOwnerViewModel.Hours,
+                    Amount = payrollOwnerViewModel.Amount,
+                    Date = payrollOwnerViewModel.Date,
+                    UserId = payrollOwnerViewModel.UserId
+                };
+
+                _context.Add(payroll1);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Owner));
+
+
+            }
+
+            ViewData["FullNames"] = new SelectList(diff_name_payrolls, "FullName", "FullName");
+            ModelState.AddModelError(string.Empty, "There was an issue adding this item into your Payroll");
+            return View();
+
+        }
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
