39584fa0466d633335a76eb848b821083ca82615
Selina Do <selina.do@student.csulb.edu>
2022-11-07 13:05:28 -0800

    add back button to checklist page

--- Controllers/ChecklistController.cs
+++ Controllers/ChecklistController.cs
@@ -1,27 +1,25 @@
-﻿using BaristaHome.Models;
+﻿using BaristaHome.Data;
+using BaristaHome.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
 
 namespace BaristaHome.Controllers
 {
     [Authorize]
     public class ChecklistController : Controller
     {
-        private readonly ILogger<ChecklistController> _logger;
-        public ChecklistController(ILogger<ChecklistController> logger)
+        private readonly BaristaHomeContext _context;
+
+        public ChecklistController(BaristaHomeContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
         public IActionResult Checklist()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
