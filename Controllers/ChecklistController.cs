630c3e6a6fd4790d964a8074c4658706c0c7fd8b
Selina Do <selina.do@student.csulb.edu>
2022-11-07 21:09:19 -0800

    create checklist popup

--- Controllers/ChecklistController.cs
+++ Controllers/ChecklistController.cs
@@ -21,5 +21,30 @@ namespace BaristaHome.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Checklist([Bind("ChecklistTitle")] Checklist checklist)
+        {
+            checklist.StoreId = Convert.ToInt32(User.FindFirst("StoreId").Value);
+            if (ModelState.IsValid)
+            {
+                //checks if entered in checklist name already exists or not
+                var existingChecklist = (from c in _context.Checklist
+                                         where c.ChecklistTitle.Equals(checklist.ChecklistTitle)
+                                         select c).FirstOrDefault();
+
+                if (existingChecklist != null)
+                {
+                    ModelState.AddModelError(string.Empty, "Checklist name already exists! Please use a different one.");
+                    return View(checklist);
+                }
+
+                _context.Add(checklist);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Checklist", "Checklist");
+            }
+            ModelState.AddModelError(string.Empty, "There was an issue creating a checklist.");
+            return View(checklist);
+        }
     }
 }
