6411a48e54a34c33f3ad9d9eb9cd995baa1c78a8
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2022-11-15 15:08:50 -0800

    Feedback and Owner adds
    Added some stuff

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -1,21 +1,26 @@
-﻿using BaristaHome.Models;
+﻿using BaristaHome.Data;
+using BaristaHome.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BaristaHome.Controllers
 {
     public class FeedbackController : Controller
     {
-        private readonly ILogger<FeedbackController> _logger;
-        public FeedbackController(ILogger<FeedbackController> logger)
-        {
-            _logger = logger;
-        }
-        public IActionResult Feedback()
-        {
-            return View();
-        }
+        /*        private readonly ILogger<FeedbackController> _logger;
+                public FeedbackController(ILogger<FeedbackController> logger)
+                {
+                    _logger = logger;
+                }*/
 
+
+        private readonly BaristaHomeContext _context;
+
+        public FeedbackController(BaristaHomeContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Worker()
         {
@@ -23,6 +28,22 @@ namespace BaristaHome.Controllers
         }
 
 
+        public async Task <IActionResult> Owner()
+        {
+            var reviews = await _context.Feedback
+                .Where(u => u.StoreId == Convert.ToInt16(User.FindFirst("StoreId").Value))
+                .OrderByDescending(u => u.FeedbackId)
+                .ToListAsync();
+
+            /*            var reviews = (from feedback in _context.Feedback
+                                       join store in _context.Store on feedback.StoreId equals store.StoreId
+                                       join user in _context.User on store.StoreId equals user.StoreId
+                                       where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
+                                       orderby feedback.FeedbackId descending
+                                       select feedback).ToList();*/
+
+            return View(reviews);
+        }
 
 
 
