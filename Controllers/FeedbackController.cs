f6cb43ff729c6551f48a8de5391b695a7bac960e
borkbunns <petter.vu2000@gmail.com>
2022-11-30 05:53:48 -0800

    fixed feedback to delete upon clicking the checkmark button instead
    remove resolve page as there's no need for it anymore

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -13,20 +13,12 @@ namespace BaristaHome.Controllers
 {
     public class FeedbackController : Controller
     {
-        /*        private readonly ILogger<FeedbackController> _logger;
-                public FeedbackController(ILogger<FeedbackController> logger)
-                {
-                    _logger = logger;
-                }*/
-
-
         private readonly BaristaHomeContext _context;
 
         public FeedbackController(BaristaHomeContext context)
         {
             _context = context;
         }
-
 
         public async Task <IActionResult> Owner()
         {
@@ -35,46 +27,23 @@ namespace BaristaHome.Controllers
                 .OrderByDescending(u => u.FeedbackId)
                 .ToListAsync();
 
-            /*            var reviews = (from feedback in _context.Feedback
-                                       join store in _context.Store on feedback.StoreId equals store.StoreId
-                                       join user in _context.User on store.StoreId equals user.StoreId
-                                       where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
-                                       orderby feedback.FeedbackId descending
-                                       select feedback).ToList();*/
-
-
-            ListDictionary descriptions = new ListDictionary();
-            foreach (var review in reviews)
-            {
-                descriptions.Add(review.Title, review.Description);
-            }
-
-            ViewData["reviews"] = descriptions;
-
             return View(reviews);
         }
 
         [HttpPost]
         public JsonResult SearchDescription(string topic)
         {
-
-            var reviews = (from f in _context.Feedback
+            // getting data for feedbacks to correctly display whichever one the user clicks on
+            var feedbacks = (from f in _context.Feedback
                            join store in _context.Store on f.StoreId equals store.StoreId
                            where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value)) 
                            select f).ToList();
 
-
-            return Json(reviews);
-
+            return Json(feedbacks);
         }
 
-
-
-
-
-
-        // POST: Resolve Feedback 
-        [HttpPost, ActionName("Resolve")]
+        // POST: Delete Feedback 
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
@@ -88,78 +57,10 @@ namespace BaristaHome.Controllers
 
             _context.Feedback.Remove(feedbackItem);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Owner));
-        }
-
-
-
-
-        // Get: Test Delete Page
-        public async Task<IActionResult> Resolve(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var storeName = (from store in _context.Store
-                             join feedback in _context.Feedback on store.StoreId equals feedback.StoreId
-                             where store.StoreId.Equals(Convert.ToInt16(User.FindFirst("StoreId").Value))
-                             select store.StoreName).ToList();
-
-            ViewBag.StoreName = storeName;
-
-
-            var feedbackItem = await _context.Feedback.FirstOrDefaultAsync(m => m.FeedbackId == id);
-
-       
-
-
-            return View(feedbackItem);
-        }
-
-
-
-
-
-
-
-
-
-        /*        // POST: Trying with convert from string to int
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async JsonResult DeleteConfirmed(string id)
-        {
-
-            var intId = Convert.ToInt32(id);
-
-            // requery item then delete
-            //var feedbackItem = await _context.Feedback.FindAsync(intId);
-            var feedbackItem = _context.Feedback.FirstOrDefault(x => x.FeedbackId == intId);
-
-*//*            if (feedbackItem == null)
-            {
-                return View("Error");
-            }*//*
-
-            _context.Feedback.Remove(feedbackItem);
-            await _context.SaveChangesAsync();
-            return Json(new { id });
             //return RedirectToAction(nameof(Owner));
+            // this is the best way i could "refresh" the page to update the feedback list once it's deleted, as the above statement doens't work idk why
+            return Json(new { redirectToUrl = Url.Action("Owner", "Feedback") });
         }
-*/
-
-
-
-
-
-
-
-
-
-
-
 
         // Views that the worker will see
         [HttpGet]
@@ -170,7 +71,7 @@ namespace BaristaHome.Controllers
 
         [HttpPost]
         public async Task<IActionResult> Worker([Bind("FeedbackId,Title,Description,StoreId,UserId")] Feedback feedback)
-        {  
+        {
             feedback.StoreId = Convert.ToInt16(User.FindFirst("StoreId").Value);
             feedback.UserId = Convert.ToInt16(User.FindFirst("UserId").Value);
 
@@ -180,24 +81,8 @@ namespace BaristaHome.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Worker", "Feedback");
             }
-                
-         
-         ModelState.AddModelError(string.Empty, "There was an issue creating an account.");
-         return View(feedback);
+            return View(feedback);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
