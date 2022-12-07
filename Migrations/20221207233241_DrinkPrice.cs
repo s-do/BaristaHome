155b360dd59cf81f5d419b831300ff8cf6f73d44
EmeraldEnvy <EmeraldEnvy500@gmail.com>
2022-12-07 15:48:55 -0800

    price for create drink

---
+++ Migrations/20221207233241_DrinkPrice.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaristaHome.Migrations
+{
+    public partial class DrinkPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Profit",
+                table: "Sale",
+                type: "decimal(16,2)",
+                precision: 16,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)",
+                oldPrecision: 16,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Drink",
+                type: "decimal(8,2)",
+                precision: 8,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Drink");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Profit",
+                table: "Sale",
+                type: "decimal(16,2)",
+                precision: 16,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)",
+                oldPrecision: 16,
+                oldScale: 2);
+        }
+    }
+}
